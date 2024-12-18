ac4d7b28fb29816d6bc155f31393f9cdac7bf59d
yonatan957 <yonatan22660@gmail.com>
2024-12-18 22:16:57 +0200

    add lat and long

--- src/models/CountryGroupsModel.ts
+++ src/models/CountryGroupsModel.ts
@@ -4,12 +4,16 @@ import { Document, model, Schema } from "mongoose";
 
 export interface ICountryGroups extends Document {
     country_txt: string;
-    groups: {gname: string, count: number}[]
+    groups: {gname: string, count: number}[];
+    lat: number;
+    lng: number;
 }
 
 const CountryGroupsSchema = new Schema<ICountryGroups>({
     country_txt: {type: String, required: true},
-    groups: [{gname: String, count: Number}]
+    groups: [{gname: String, count: Number}],
+    lat: {type: Number, required: true},
+    lng: {type: Number, required: true}
 })
 
 export default model<ICountryGroups>("CountryGroups", CountryGroupsSchema);
