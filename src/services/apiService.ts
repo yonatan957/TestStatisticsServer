d90f831ccc8cdaf884c5d2541bff0ba947bba7c1
yonatan957 <yonatan22660@gmail.com>
2024-12-19 00:05:15 +0200

    create functions for 2 first endpoints

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,12 +1,32 @@
 import AttackTypeModel from "../models/AttackTypeModel";
+import StateAttacksModel from "../models/StateAttacksModel";
 
 export const getAttackTypes = async ()=>{
-    const attackTypes = await AttackTypeModel.find({}).sort({ countKill: -1 });
+    const attackTypes = await AttackTypeModel.find({}).lean().sort({ countKill: -1 });
     return attackTypes
 }
 
 export const getCasualtyRegions = async ()=>{
+    const stateAttacks = await StateAttacksModel.aggregate([
+        {
+            $project: {
+                country_txt: 1,
+                count: 1,
+                countKill: 1,
+                countWound: 1,
+                latitude: 1,
+                longitude: 1,
+                averageCasualties: {
+                    $avg: ["$countKill", "$countWound"]
+                }
+            }
+        },
+        {
+            $sort: { averageCasualties: -1 }
+        }
+    ]);
 
+    return stateAttacks
 }
 
 export const getIncidentTrendsService = async ()=>{
