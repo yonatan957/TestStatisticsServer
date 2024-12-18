867608d635212500e22c9ac9deec919d9aa9f0f8
yonatan957 <yonatan22660@gmail.com>
2024-12-18 22:16:17 +0200

    add ;

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,32 @@
-import express from 'express'
-import 'dotenv/config'
-import { conectToMongo } from './config/DB'
-import cors from 'cors'
-import http from 'http'
-import { Server } from 'socket.io'
-import { handleSocketConnection } from './socket/io'
-import { addMany } from './services/sidService'
-const data = require('../initialData.json')
+import express from "express";
+import "dotenv/config";
+import { conectToMongo } from "./config/DB";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+import { handleSocketConnection } from "./socket/io";
+import { initProject } from "./services/sidService";
+import { IEvent } from "./models/EventModel";
 
-const PORT = process.env.PORT
-const app = express()
-const httpServer:http.Server = http.createServer(app)
-export const io = new Server(httpServer,{
-    cors:{
-        origin: '*',
-        methods:'*'
-    }
-})
+const PORT = process.env.PORT;
+const app = express();
+const httpServer: http.Server = http.createServer(app);
+export const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: "*",
+  },
+});
 
-io.on('connection', handleSocketConnection)
+io.on("connection", handleSocketConnection);
 
-conectToMongo()
+conectToMongo();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-
-addMany(data)
+initProject();
 
 httpServer.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
-})
+  console.log(`Server running on http://localhost:${PORT}`);
+});
